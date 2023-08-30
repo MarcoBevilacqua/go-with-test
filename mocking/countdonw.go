3f41c6902eeb52268a3d981cbdaa9cc993141260
Marco <dontstop.marco@gmail.com>
2023-08-30 12:46:15 +0200

    mocking

--- mocking/countdonw.go
+++ mocking/countdonw.go
@@ -14,12 +14,6 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep((1 * time.Second))
-}
-
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
@@ -31,6 +25,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
