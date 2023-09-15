040c8ef63fdcac5e88d33639bfd4131ab9a83ab9
Marco <dontstop.marco@gmail.com>
2023-09-15 13:11:47 +0200

    generics

---
+++ generics/go.mod
@@ -0,0 +1,3 @@
+module github.com/MarcoBevilacqua/generics
+
+go 1.21.0
---
+++ generics/stacks_test.go
@@ -0,0 +1,10 @@
+package generics_test
+
+import (
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+	})
+}
